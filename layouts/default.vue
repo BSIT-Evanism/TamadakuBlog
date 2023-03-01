b73a6618186ca29edda19db983fa46a18b093ec1
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-03-02 00:30:35 +0800

    2nd commit 2nd testing POTA

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header>
+            <div class="border-dotted b-2">
+                <div class="flex justify-center gap-4 p-4">
+                    <li class="font-bold text-slate-700 hover-outline-groove animate-all 250ms ease-in">
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li class="font-bold text-slate-700 hover-outline-groove animate-all 250ms ease-in">
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li class="font-bold text-slate-700 hover-outline-groove animate-all 250ms ease-in">
+                        <NuxtLink to="/people">People</NuxtLink>
+                    </li>
+                    <li class="font-bold text-slate-700 hover-outline-groove animate-all 250ms ease-in">
+                        <NuxtLink to="/articles">Articles</NuxtLink>
+                    </li>
+                </div>
+            </div>
+        </header>
+        <div class="p-4 mx-auto">
+            <slot />
+        </div>
+        <footer class="flex bg-coolgray w-screen h-5">
+            <div class="flex justify-center align-items gap-2 mx-auto"><marquee class=" w-full" width="100%" behavior="string" direction="left">
+                <span>Testing lang po</span>
+                <span>Testing uli HUHUHU</span>
+                <span>Testing uli HUHUHU</span>
+            </marquee></div>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+            <div>Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Aliquid facilis iste provident quam eveniet similique minima iusto veritatis id magnam!</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
