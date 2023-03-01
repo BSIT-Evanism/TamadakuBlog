e9ef3645b97c1f8462cdc8d19b735e4f32360c66
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-03-02 00:30:25 +0800

    Co-authored-by: Menchie <menchb@users.noreply.github.com>

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus corrupti ab placeat odit. Ipsum, quasi officiis natus deserunt vitae rerum minima provident voluptatem molestias labore quae nam, odit, asperiores hic.
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
